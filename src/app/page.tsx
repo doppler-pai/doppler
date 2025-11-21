93065885267d769e661c94604215b7ff7554a119
Jakub Mazurek <kuba@Jakubs-MacBook-Air.local>
2025-11-21 15:34:25 +0100

    chore: initilaized nextjs project, tailwind colors, shadcn library

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Header 1</h1>
+      <h2>Header 2</h2>
+      <h3>Header 3</h3>
+      <h4>Header 4</h4>
+      <p>
+        Paragraph Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+        in quam ligula. Cras massa ligula, pulvinar pulvinar metus placerat,
+        pulvinar fermentum mauris. Integer molestie est augue, non efficitur
+        neque
+      </p>
+      <small>
+        Small text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+        in quam ligula. Cras massa ligula, pulvinar pulvinar metus placerat,
+        pulvinar fermentum mauris. Integer molestie est augue, non efficitur
+        neque
+      </small>
+
+      <br></br>
+      <Button variant={"ghost"}> Click me! </Button>
+      <Button variant={"outline"}> Click me! </Button>
+      <Button variant={"secondary"}> Click me! </Button>
+
+      <Button> Click me! </Button>
+    </>
+  );
+}
