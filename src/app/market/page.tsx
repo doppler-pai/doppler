ffa064171d84e36743f383293d63d1b6189f6ee2
MichalK252 <u39_mickar_lbn@technischools.com>
2025-11-29 16:52:31 +0100

    feat: add user currency fetching and dispay on market page

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,19 +1,46 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { auth, db } from '@/shared/lib/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+
+
 import { Button } from '@/shared/components/ui/button';
 import { Separator } from '@/shared/components/ui/separator';
 import { PackCard } from '@/features/market/PackCard';
 import Link from 'next/link';
 
 export default function Market() {
+  const [coins, setCoins] = useState<number | null>(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        setCoins(null);
+        return;
+      }
+  
+      const ref = doc(db, 'users', user.uid);
+      const snap = await getDoc(ref);
+
+      console.log("DOC DATA:", snap.data());
+  
+      if (snap.exists()) {
+        setCoins(snap.data().currency ?? 0);
+      }
+    });
+  
+    return () => unsubscribe();
+  }, []);
+  
+
   return (
     <div className="w-full">
       <div className="w-full h-16 flex items-center px-12 justify-between">
         <h1 className="mt-10 ml-12">Dopple Market</h1>
         <div className="flex items-center gap-8 mt-10 mr-12">
-          <Button variant={'outline'}>
-            value
+          <Button variant={'outline'} className="flex items-center gap-1">
+            {coins !== null ? coins : '...'}
             <img src="/logo/logo.png" className="w-4 h-4" />
           </Button>
           <Button variant={'outline'}>Skins owned: </Button>
